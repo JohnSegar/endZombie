110be27e5ec24319ed580f411465602a17e5b2de
JohnSegar <johnjamessegar@hotmail.co.uk>
2016-11-21 23:24:46 +0000

    views set up

--- js/app.js
+++ js/app.js
@@ -7,7 +7,17 @@ function MainRouter($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: "/",
       templateUrl: "/js/views/home.html",
-    });
+    })
+    .state('game', {
+      url: "/game",
+      templateUrl: "/js/views/game.html"
+    })
+    .state('winner', {
+      url: "/game/winner",
+      templateUrl: "/js/views/winner.html"
+    })
+
+    ;
 
 
   $urlRouterProvider.otherwise('/');
