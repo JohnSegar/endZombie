cb609a3b2a2686f2bf1597104063b1aabde36477
JohnSegar <johnjamessegar@hotmail.co.uk>
2016-11-21 23:13:33 +0000

    added json

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-angular.module('endZombie', []);
+angular.module('endZombie', ['ui.router'])
+.config(MainRouter);
+
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "/js/views/home.html",
+    });
+
+
+  $urlRouterProvider.otherwise('/');
+}
